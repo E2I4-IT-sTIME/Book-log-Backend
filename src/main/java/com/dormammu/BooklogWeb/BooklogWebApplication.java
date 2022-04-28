85b2dd18dd392086a9f30daae5d9c227800c5cbe
kyj <khkkgon0118@gmail.com>
2022-04-29 00:22:28 +0900

    first commit

---
+++ src/main/java/com/dormammu/BooklogWeb/BooklogWebApplication.java
@@ -0,0 +1,13 @@
+package com.dormammu.BooklogWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklogWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dormammu/BooklogWeb/BooklogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dormammu.BooklogWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
