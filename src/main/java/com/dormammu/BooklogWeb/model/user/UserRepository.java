3580de73c77aa2fe356e536f6a7dcfc8e36ed17e
kyj <khkkgon0118@gmail.com>
2022-05-03 04:53:37 +0900

    jwt 테스트

--- src/main/java/com/dormammu/BooklogWeb/model/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dormammu.BooklogWeb.model.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
-}
