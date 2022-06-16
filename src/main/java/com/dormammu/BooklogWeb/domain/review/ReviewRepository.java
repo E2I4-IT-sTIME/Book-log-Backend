bfa52995b783abdfc7e365cd194019d5ebe0516a
kyj <khkkgon0118@gmail.com>
2022-06-17 02:39:54 +0900

    내 서평 조회 api

---
+++ src/main/java/com/dormammu/BooklogWeb/domain/review/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.dormammu.BooklogWeb.domain.review;
+
+import com.dormammu.BooklogWeb.domain.portfolio.Portfolio;
+import com.dormammu.BooklogWeb.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // 생략 가능
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    List<Review> findByUser(User user);
+    List<Review> findByUserId(int userId);
+}
---
+++ src/main/java/com/dormammu/BooklogWeb/dto/ReviewListRes.java
@@ -0,0 +1,20 @@
+package com.dormammu.BooklogWeb.dto;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReviewListRes {
+    private int userId;
+    private String nickname;
+
+    List<ReviewRes> reviewResList;
+
+}
