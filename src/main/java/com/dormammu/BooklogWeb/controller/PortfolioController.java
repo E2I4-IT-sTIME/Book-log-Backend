7a4949ae5dcceea588deb09e76a8ff80d4cf9955
kyj <khkkgon0118@gmail.com>
2022-06-19 01:49:52 +0900

    포트폴리오 생성 API

--- src/main/java/com/dormammu/BooklogWeb/controller/PortfolioController.java
+++ src/main/java/com/dormammu/BooklogWeb/controller/PortfolioController.java
@@ -4,12 +4,11 @@ import com.dormammu.BooklogWeb.config.auth.PrincipalDetails;
 import com.dormammu.BooklogWeb.domain.portfolio.Portfolio;
 import com.dormammu.BooklogWeb.domain.user.User;
 import com.dormammu.BooklogWeb.domain.user.UserRepository;
+import com.dormammu.BooklogWeb.dto.PostPortfolioReq;
 import com.dormammu.BooklogWeb.service.PortfolioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +19,19 @@ public class PortfolioController {
     private final PortfolioService portfolioService;
     private final UserRepository userRepository;
 
+    @PostMapping("/auth/portfolio")
+    public String createPortfolio(@RequestBody PostPortfolioReq postPortfolioReq, Authentication authentication) {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        if (postPortfolioReq.getUserId() == principalDetails.getUser().getId()) {
+            portfolioService.createPortfolio(principalDetails.getUser(), postPortfolioReq);
+
+            return "포트폴리오 생성 완료";
+        }
+        return null;
+    }
+
     // 내 포트폴리오 조회 페이지
-    @GetMapping("/api/v1/user/{id}/porfols")
+    @GetMapping("/api/user/{id}/porfols")
     public List<Portfolio> myPortfolioList(@PathVariable int id, Authentication authentication) {
         User user = userRepository.findById(id);
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
