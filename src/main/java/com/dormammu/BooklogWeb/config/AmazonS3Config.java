d3a0ca927eaef29d3c7a0bbd6395d2f0cfe7318a
ChaeyeonHan <kate2236e@naver.com>
2022-07-25 01:50:06 +0900

    이미지 서버 구축

---
+++ src/main/java/com/dormammu/BooklogWeb/config/AmazonS3Config.java
@@ -0,0 +1,33 @@
+package com.dormammu.BooklogWeb.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {  // 설정 값 등록 파일
+
+    @Value(("${cloud.aws.credentials.access-key}"))
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    // yml파일에 작성한 값을 읽어와서 AmazonS3Client객체를 만들어 bean으로 주입
+    @Bean
+    public AmazonS3Client amazonS3Client(){
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+
+}
