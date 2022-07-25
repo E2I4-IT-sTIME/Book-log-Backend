bc844d2f1d6044460275315a6a5e863a6ee9900b
ChaeyeonHan <kate2236e@naver.com>
2022-07-26 01:05:38 +0900

    이미지 테ã스트

--- src/main/java/com/dormammu/BooklogWeb/dto/FileUploadResponse.java
+++ src/main/java/com/dormammu/BooklogWeb/dto/FileUploadResponse.java
@@ -1,15 +1,15 @@
-//package com.dormammu.BooklogWeb.dto;
-//
-//import lombok.Data;
-//
-//@Data
-//public class FileUploadResponse {
-//
-//    String fileName;
-//    String url;
-//
-//    public FileUploadResponse(String fileName, String url) {
-//        this.fileName = fileName;
-//        this.url = url;
-//    }
-//}
+package com.dormammu.BooklogWeb.dto;
+
+import lombok.Data;
+
+@Data
+public class FileUploadResponse {
+
+    String fileName;
+    String url;
+
+    public FileUploadResponse(String fileName, String url) {
+        this.fileName = fileName;
+        this.url = url;
+    }
+}
--- src/main/java/com/dormammu/BooklogWeb/service/S3Uploader.java
+++ src/main/java/com/dormammu/BooklogWeb/service/S3Uploader.java
@@ -9,6 +9,7 @@ import com.dormammu.BooklogWeb.domain.meeting.Meeting;
 import com.dormammu.BooklogWeb.domain.meeting.MeetingRepository;
 import com.dormammu.BooklogWeb.domain.user.User;
 import com.dormammu.BooklogWeb.domain.user.UserRepository;
+import com.dormammu.BooklogWeb.dto.FileUploadResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,26 +37,26 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String upload(int meetingId, MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
-
-//        Path filepath = Paths.get(dirName.toString(), multipartFile.getOriginalFilename());
-//        multipartFile.transferTo(filepath);
-//        return upload(multipartFile, dirName);
-        return upload(meetingId, uploadFile, dirName);
-    }
-
-    // s3로 파일 업로드하기
-    private String upload(int meetingId, File uploadFile, String dirName) {
-        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
-        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
-        Meeting meeting = meetingRepository.findById(meetingId);
-        meeting.setImage(uploadImageUrl);
-        removeNewFile(uploadFile);
-
-        return "모임 생성 및 사진 저장 성공";
-    }
+//    public String upload(int meetingId, MultipartFile multipartFile, String dirName) throws IOException {
+//        File uploadFile = convert(multipartFile)
+//                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+//
+////        Path filepath = Paths.get(dirName.toString(), multipartFile.getOriginalFilename());
+////        multipartFile.transferTo(filepath);
+////        return upload(multipartFile, dirName);
+//        return upload(meetingId, uploadFile, dirName);
+//    }
+//
+//    // s3로 파일 업로드하기
+//    private String upload(int meetingId, File uploadFile, String dirName) {
+//        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();   // S3에 저장된 파일 이름
+//        String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
+//        Meeting meeting = meetingRepository.findById(meetingId);
+//        meeting.setImage(uploadImageUrl);
+//        removeNewFile(uploadFile);
+//
+//        return "모임 생성 및 사진 저장 성공";
+//    }
 
 //    public String uploadProfile(int userId, MultipartFile multipartFile, String dirName) throws IOException {
 //        File uploadFile = convert(multipartFile)
@@ -80,6 +81,23 @@ public class S3Uploader {
 //        return "회원가입 및 유저 프로필 사진 등록 완료";
 //    }
 //
+    /*  사진 하나 업로드하기 */
+    public FileUploadResponse upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+
+        return upload(uploadFile, dirName);
+    }
+
+    private FileUploadResponse upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+
+        return new FileUploadResponse(fileName, uploadImageUrl);
+    }
+
+
     // 업로드하기
     private String putS3(File uploadFile, String fileName){
         amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
@@ -100,9 +118,7 @@ public class S3Uploader {
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
                 fos.write(file.getBytes());
-                System.out.println("변환");
             }
-            System.out.println("오류");
             return Optional.of(convertFile);
         }
 
