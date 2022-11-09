4be88a95e9893a21ef1eb81e933f987d22c32089
kyj <khkkgon0118@gmail.com>
2022-11-09 23:50:22 +0900

    feat : github action 설정파일

--- src/main/java/com/dormammu/BooklogWeb/domain/review/PortfolioReviewRepository.java
+++ src/main/java/com/dormammu/BooklogWeb/domain/review/PortfolioReviewRepository.java
@@ -8,9 +8,5 @@ import java.util.List;
 
 @Repository // 생략 가능
 public interface PortfolioReviewRepository extends JpaRepository<PortfolioReview, Integer> {
-    PortfolioReview findByPofolioIdAndReviewId(int portfolio_id, int review_id);
-    List<PortfolioReview> findByPofolioId(int portfolio_id);
-    Boolean existsByPofolioIdAndReviewId(int portfolio_id, int review_id);
-
     void deleteByPortfolioId(int portfolio_id);
 }
