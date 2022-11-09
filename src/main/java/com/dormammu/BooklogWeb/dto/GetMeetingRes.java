8984b2ff0fcee76a043c13446c035423a025c8eb
ChaeyeonHan <kate2236e@naver.com>
2022-11-09 22:30:55 +0900

    feat: 독서모임 상세페이지 api

--- src/main/java/com/dormammu/BooklogWeb/dto/GetMeetingRes.java
+++ src/main/java/com/dormammu/BooklogWeb/dto/GetMeetingRes.java
@@ -13,15 +13,26 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class GetMeetingRes {
+//    private int id; // meeting_id
+//    private String info;
+//    private String image;
+//    private String name;
+//    private int max_num;
+//    private int cur_num;
+//    private boolean onoff;
+//    private String username;
+//    private String email;
+//
+//    List<String> tags;
+
+
     private int id; // meeting_id
-    private String info;
+    private String isbn;  // 책 정보
+    private String name;  // 모임 이름
     private String image;
-    private String name;
+    private boolean onoff;  // 온라인, 오프라인
+    private String username;
     private int max_num;
     private int cur_num;
-    private boolean onoff;
-    private String username;
-    private String email;
-
     List<String> tags;
 }
