fa3ca6be5c75179b5080c1728dc6cea0ea6514d6
ChaeyeonHan <kate2236e@naver.com>
2022-11-14 15:17:54 +0900

    feat: swagger 추가적용

--- src/main/java/com/dormammu/BooklogWeb/dto/GetUserQnAListRes.java
+++ src/main/java/com/dormammu/BooklogWeb/dto/GetUserQnAListRes.java
@@ -1,5 +1,6 @@
 package com.dormammu.BooklogWeb.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.List;
@@ -10,10 +11,15 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class GetUserQnAListRes {
+    @ApiModelProperty(example = "유저 이름")
     private String username;
+
+    @ApiModelProperty(example = "유저 이메일")
     private String email;
 
+    @ApiModelProperty(example = "답변 리스트")
     private List<String> answers;
 
+    @ApiModelProperty(example = "질문 리스트")
     private List<String> questions;
 }
