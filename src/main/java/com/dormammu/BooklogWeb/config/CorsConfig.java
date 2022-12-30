a4a999415f46d4e60e62fa0546a520e410b25bb3
myeondii <khkkgon0118@gmail.com>
2022-12-31 00:16:51 +0900

    api test

--- src/main/java/com/dormammu/BooklogWeb/config/CorsConfig.java
+++ src/main/java/com/dormammu/BooklogWeb/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);  // 내 서버가 응답할때 json을 자바스크립트에서 처리할 수 있게 설정
-        config.addAllowedOrigin("*");  // 모든 ip에 응답을 허용
+        config.addAllowedOriginPattern("*");  // 모든 ip에 응답을 허용
         config.addAllowedHeader("*");  // 모든 header에 응답을 허용
         config.addAllowedMethod("*");  // 모든 post, get, put, delete, patch를 허용ㅇ
 
