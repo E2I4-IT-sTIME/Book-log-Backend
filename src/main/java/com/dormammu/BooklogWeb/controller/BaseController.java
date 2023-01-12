09e2a4e6185f58626ec7602312176b16d0adc222
kyj <khkkgon0118@gmail.com>
2023-01-13 01:56:24 +0900

    beanstalk 배포

---
+++ src/main/java/com/dormammu/BooklogWeb/controller/BaseController.java
@@ -0,0 +1,16 @@
+package com.dormammu.BooklogWeb.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+    @GetMapping("/")
+    public ResponseEntity getServiceName() {
+        ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
+        return responseEntity;
+    }
+}
